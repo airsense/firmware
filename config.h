7fd53ac3e84bc36ffb8d00b100456ff210831e25
harsh <harsh.bhatt@me.com>
2016-03-10 00:34:26 -0500

    intermediate commit
    using hardware serial

---
+++ config.h
@@ -0,0 +1,3 @@
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
---
+++ firmware.ino
@@ -0,0 +1,15 @@
+#include "config.h"
+#include "Adafruit_FONA.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println(FONA_RX);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
